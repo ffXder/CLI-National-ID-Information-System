65f59f0567aab9d89ab84608815cf1330a73b118
ffXder <fjfajardo0624@gmail.com>
2024-12-11 10:42:18 +0800

    custom exception

--- InputExitException.java
+++ InputExitException.java
@@ -1,3 +1,4 @@
+//this is custom exception which handles the input of user to EXIT
 class InputExitException extends RuntimeException {
     public InputExitException() {
         super("Exiting...");
